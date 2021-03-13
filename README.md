755eb2a46af3aeaf0061a172cddf8ee11ee53a31
aaw0151 <alexanderwilliams2@my.unt.edu>
2021-03-13 12:54:44 -0600

    Updated app.js with mysql. Updated index.html with example format. Updated README

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 This is the project that was submitted for the UNT Hackathon 2021.
 
-
+Usage:
+Run server using Node.js. Use "node app.js" to run.
 
 Contributors:
 
--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 //Constants
 var http = require('http');
 var fs = require('fs');
-//var mysql = require('mysql');
+var mysql = require('mysql');
 const hostname = "127.0.0.1";
 const port = 8000;
+
+//mysql Information
+var mysqlcon = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "mydb"
+});
+mysqlcon.connect(function(err){
+	if(err) throw err;
+	console.log("mySQL Database Connected");
+});
 
 //Main server
 const server = http.createServer(function (req, res) {
 	fs.readFile('index.html', function(err, data) {
 		if(err) throw err;
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
+		res.write(data); //writing index.html
 		return res.end();
 	});
 });
